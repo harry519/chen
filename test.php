f833a157b26517551476929e30c93e8c29106e7c
chenshihao <850229004@qq.com>
2018-03-07 23:14:47 +0800

    第一次提交

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+
+// phpinfo();
+// echo '<pre>';
+
+// var_dump($_SERVER);
+
+
+// echo '</pre><br/>';
+// var_dump($_SERVER['PHP_SELF']);
+// $a = '/panth/';
+// $a = trim($a,'/');
+// echo $a;
+// $b = 'a,b,c,d';
+// $b = explode(',', $b,2);
+// var_dump($b);
+// echo '-----------------------------';
+// var_dump(DIRECTORY_SEPARATOR);
+// $a = (int)'亚';
+// echo $a.'<br/>';
+// var_dump($a);
+
+$a = '1,2,3,4,5,';
+$b = trim($a,',');
+var_dump($a);
+echo '<br/>';
+var_dump($b);
